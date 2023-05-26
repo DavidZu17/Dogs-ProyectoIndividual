a7d4f126c91ae67ffffb04117d68f5ba92f6cac7
DavidZu17 <david_zu.17@hotmail.com>
2023-05-26 00:03:19 -0500

    Correcion de metodos get y post y carga de datos sin tener todos los dog en la base de datos interna

--- api/src/Controllers/dogsController.js
+++ api/src/Controllers/dogsController.js
@@ -1,15 +1,41 @@
-
-const { Dog, Temperament } = require('../db');
+require('dotenv').config();
+const axios = require('axios');
+const {API_KEY , API_URL ,ID_BASE_BD } = process.env ;
+const { Dog , Temperament } = require('../db');
 const { Op } = require("sequelize");
+let numBaseIdBD = parseInt(ID_BASE_BD) ;
+
+
+
 
 const getDogs = async(req , res) => {
     try {  
-        //Consulto mi base datos de dogs y retorno todas las razas
-        let listaDeRazas = await Dog.findAll();
-        if(!listaDeRazas) throw new Error('No hay razas por consultar')
+        
+        const { data: allDogsFromApi } = await axios(`${API_URL}/?api_key=${API_KEY}`);
+        if(!allDogsFromApi) throw new Error('Error en la Carga de datos');
+        
+        //Se crea los dog con los atributos necesarios y se agrega al array a retornar
+        let listaDeRazas = [];
+        allDogsFromApi.forEach( async(dogFromApi) => {
+            const newDog ={                
+                    id : dogFromApi.id,                
+                    name : dogFromApi.name,
+                    image: dogFromApi.image.url,
+                    height: dogFromApi.height.metric,
+                    weight : dogFromApi.weight.metric,
+                    age : dogFromApi.life_span,                
+            }
+            listaDeRazas.push(newDog);
+        })
 
-        return res.status(200).json(listaDeRazas);
+        //Se consulta en la base de datos si hay dog registrados.
+        let listRazasBD = await Dog.findAll();
 
+        //Si no hay en la base de datos retorna solo la de la Api
+        if(listRazasBD.lenght === 0) return res.status(200).json(listaDeRazas);
+        
+        //En caso contrario junta las dos bases de datos y las retorna
+        return res.status(200).json(listaDeRazas.concat(listRazasBD));
     } catch (error) {
         return res.status(400).json({error: error.message})
     }
@@ -20,17 +46,53 @@ const getDogsById = async ( req , res ) =>{
         //Se busca id pasado por parametro dentro de la base de datos jumto con su relaciones
         // con la tabla Temperament , se retorna el dog con todos sus atributos y sus relaciones.
         let { idRaza } = req.params;
-        idRaza = parseInt(idRaza);
-        if(isNaN(idRaza)) throw new Error('El id debe de ser un numero')
 
-        
-        const dogFound = await Dog.findByPk(idRaza, {include: [{
+        //Se parsea el numero por parametro si  no lo es se lanza el error de dato incorrecto
+        idRaza = parseInt(idRaza);
+        if(isNaN(idRaza)) throw new Error('El id debe de ser un numero');
+
+
+        //Se busca primero el Dog en la base de datos con el id incluyendo sus temperamentos
+        let dogFound = await Dog.findByPk(idRaza, {include: [{
             model: Temperament,
             through: {
                 attributes: []
             }
         }]})
-        if(!dogFound) throw new Error(`Dog por id:${idRaza} no se encuentra en la base de datos`)
+        //Si no exite en la base de datos lo buscamos en la Api
+        if(!dogFound){
+            //Se carga la lista de dog
+            const { data: allDogsFromApi } = await axios(`${API_URL}/?api_key=${API_KEY}`);
+            //Se recorre el arreglo hasta encontrar el id especificado
+            for (let i = 0; i < allDogsFromApi.length; i++) {
+                if(allDogsFromApi[i].id === idRaza){
+                    dogFound = allDogsFromApi[i];
+                    break;
+                }
+            }
+            if(!dogFound) throw new Error(`Dog por id:${idRaza} no se encuentra en la base de datos`)
+            //Se crea un Dog con los atributos necesarios y sus temperamentos
+            const dogApiReturn = {
+                    id : dogFound.id,                
+                    name : dogFound.name,
+                    image: dogFound.image.url,
+                    height: dogFound.height.metric,
+                    weight : dogFound.weight.metric,
+                    age : dogFound.life_span,   
+                    temperaments :dogFound.temperament
+            }
+            
+            return res.status(200).json(dogApiReturn);
+
+        }
+        //Si encuentra dog en la base de datos continua con el proceso
+        //Se agrega sus temperamentos en formato String name para su facil acceso.
+        const temperamentsDog = dogFound.temperaments ;
+        dogFound.temperaments = '';
+        temperamentsDog.forEach( (temperament) => {
+            dogFound.temperaments += `${temperament.name},`;
+        })        
+
         return res.status(200).json(dogFound);
 
     } catch (error) {
@@ -44,21 +106,47 @@ const dogByName = async ( req , res ) =>{
         // por el name especificado, retorna las razas de perros con los name que contengan
         // ese name dentro o el mismo, sin importar si es minuscula o mayuscula.
         const { name } = req.query;
-
         if(!name) throw new error('Datos incompletos, ingresar name para realizar su busqueda')
-        const dogFound = await Dog.findAll({
+        
+        const  dogsFoundApi = [];
+            //Se carga la lista de dog
+            const { data: allDogsFromApi } = await axios(`${API_URL}/?api_key=${API_KEY}`);
+            //Se recorre el arreglo hasta encontrar los Dogs con el name especificado
+            for (let i = 0; i < allDogsFromApi.length; i++) {
+                
+                if( allDogsFromApi[i].name.toLowerCase().includes(name.toLowerCase()) ){
+                    //Se organiza en un nuevo objeto los Dogs con la info necesaria
+                    const dogNameInclude ={
+                        id : allDogsFromApi[i].id,                
+                        name : allDogsFromApi[i].name,
+                        image: allDogsFromApi[i].image.url,
+                        height: allDogsFromApi[i].height.metric,
+                        weight : allDogsFromApi[i].weight.metric,
+                        age : allDogsFromApi[i].life_span,   
+                    }
+                    //Se agregan en en la lista a retornar
+                    dogsFoundApi.push(dogNameInclude)
+                }
+            }
+        //Se busca en la base de datos coincidencias
+        const dogFoundBD = await Dog.findAll({
             where:{
                 name : {
                     [Op.iLike]: `%${name}%`
                 }
             }
         })
-
-        if(!dogFound) throw new Error(`No se ha encontrado ningun Dog con el name:${name} especificado`)
-
-        return res.status(200).json(dogFound);
-
-
+        //Error si no se encuentra en ninguna base de datos
+        if(dogFoundBD.length === 0 && dogsFoundApi.length === 0) throw new Error(`El Dog con el name: ${name}, no se encuentra registrado`)
+        //Si en las dos bases de datos Api y BD , huieron coincidencias se retorna los dos arreglos juntos
+        if(dogFoundBD.length !== 0 && dogsFoundApi.length !== 0)  return res.status(200).json(dogFoundBD.concat(dogsFoundApi));
+        //Si solo en una base de datos se encontro coincidencias retorna la correspondiente
+        if(dogFoundBD.length === 0 || dogsFoundApi.length === 0){
+            if(dogFoundBD.length !== 0 )
+                return res.status(200).json(dogFoundBD);
+            else
+                return res.status(200).json(dogsFoundApi);
+        }
     } catch (error) {
         return res.status(400).json({error: error.message})
     }
@@ -68,40 +156,47 @@ const postDog = async( req , res ) => {
     try {
         /* Se realiza la creacion del nuevo Dog para cargarlo en la base de datos con su respectiva
             relaciones con temperaments
-
         */
         const { name, image, height, weight, age , temperaments } = req.body;
        //Se verifica que los datos hallan sido ingresados en su totalidad y no vacios
         if(!name || !height || !weight|| !image || !age || !temperaments
-           || !name.lenght === 0 || !name.height === 0 || !name.weight === 0 
-           || !name.image === 0 || !name.age === 0 || !name.temperament === 0  ) 
+           || name.lenght === 0 || height.lenght === 0 || weight.lenght === 0 
+           || image.lenght === 0 || age.lenght === 0 || temperaments.lenght === 0  ) 
            return res.status(400).send('Faltan datos para el registro del nuevo Dog');
         
-        //Se toma como referencia la cnatidad de Dogs registrados y se genera una key
-        // unica
-        let cantidadTotalDog = await Dog.count() ;
-        cantidadTotalDog++;        
-        let isId = await Dog.findByPk(cantidadTotalDog);
-        while(isId){
-            isId = await Dog.findByPk(cantidadTotalDog);
-            cantidadTotalDog++;
+        //Se carga la lista de dog de Api
+        const { data: allDogsFromApi } = await axios(`${API_URL}/?api_key=${API_KEY}`);
+        //Se recorre el arreglo hasta encontrar si hay un name igual para no tener repetidos, si no lo hay se puede agregar el Dog nuevo
+        let dogFound = [];
+        for (let i = 0; i < allDogsFromApi.length; i++) {
+            if(allDogsFromApi[i].name === name){
+                dogFound = allDogsFromApi[i];
+                break;
+            }
         }
+        if(dogFound.length !== 0) throw new Error(`El dog con name: ${name} ya se encuentra registrado 456`);
 
-        const [ newDog ] = await Dog.findOrCreate({
+
+
+        //Al no encontrarlo en la base de datos de la Api sigue buscando en la base de datos interna
+        //Se busca o crea en caso de no encontrarlo un nuevo Dog con sus respectivos atributos
+        const  newDog  = await Dog.findOrCreate({
             where:{
-                name,
+                name                
+            },
+            defaults:{
+                id : numBaseIdBD,
                 image,
                 height,
                 weight,
                 age
-            },
-            defaults:{
-                id : cantidadTotalDog
             }           
         })
-        
-        if(newDog[1]=== false) throw new Error(`El dog con name: ${name} ya se encuentra creado`);
-       //se organiza los temperamentos en lista para agregarlos uno por uno
+        //Error si ya existe el usuario ingresado por formulario
+        if(newDog[1]=== false) throw new Error(`El dog con name: ${name} ya se encuentra registrado`);
+       //Se incrementa la variable BASE_ID_BD para un posterior registro nuevo
+        numBaseIdBD++;
+        //se organiza los temperamentos en lista para agregarlos uno por uno
         const listaDeTemperaments = temperaments.split(',');
         //Se crea los temperamentos
         listaDeTemperaments.forEach( async (temperament) => {
@@ -111,13 +206,9 @@ const postDog = async( req , res ) => {
                 }
             })
             // Se agrega la relacion del temperamento con el Dog nuevo
-            await newDog.addTemperament(newTemperament);
-        })
-        
-        return res.status(200).json(newDog);
-
-
-
+            await newDog[0].addTemperament(newTemperament);
+        })        
+        return res.status(200).json(newDog[0]);
     } catch (error) {
         return res.status(400).json({error: error.message})
     }
--- api/src/Utils/saveInfoInDB.js
+++ api/src/Utils/saveInfoInDB.js
@@ -1,43 +1,27 @@
 require('dotenv').config();
 const axios = require('axios');
 const {API_KEY , API_URL } = process.env ;
-const { Dog , Temperament } = require('../db');
+const { Temperament } = require('../db');
 
+//Metodo que al iniciar el servidor se encarga de generar automaticamente en la base de datos
+// la tabla de temperamentos disponibles que tiene cada Dog en la Api para su uso respectivo.
 const saveInfoInDB = async(req , res) => {
     try {
         const { data: allDogsFromApi } = await axios(`${API_URL}/?api_key=${API_KEY}`);
 
         if(!allDogsFromApi) throw new Error('Error en la Carga de datos');
-        
-        allDogsFromApi.forEach( async(dogFromApi) => {
-            const [newDog] = await Dog.findOrCreate({
-                where:{
-                    id : dogFromApi.id,
-                },
-                defaults:{
-                    name : dogFromApi.name,
-                    image: dogFromApi.image.url,
-                    height: dogFromApi.height.metric,
-                    weight : dogFromApi.weight.metric,
-                    age : dogFromApi.life_span,
-                }
-            })
-            //Se crea temperamento
-            const temperamentFromDogList = dogFromApi.temperament?.split(', ') ;
+
+        for (let i = 0; i < allDogsFromApi.length; i++) {
+            const temperamentFromDogList = allDogsFromApi[i].temperament?.split(', ') ;
 
             temperamentFromDogList?.forEach( async (temperamentDog) => {
-                const [newTemperament] = await Temperament.findOrCreate({
+                const newTemperament = await Temperament.findOrCreate({
                     where: {
                         name : temperamentDog
                     }
                 })
-                //crear relacion entre dog y temperamento
-                await newDog.addTemperament(newTemperament);
             })
-            
-        });
-        return true;
-        
+        }        
     } catch (error) {
         return error;
     }
